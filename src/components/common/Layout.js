257bdb30e5f4350e956dcc24f7ec330ee2738c2f
Mike Bychkowski <mbychkowski@gmail.com>
2020-07-18 19:20:54 -0400

    Minimize style

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -37,30 +37,23 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     {/* The main header section on top of the screen */}
                     <header className="site-head" style={{ ...site.cover_image && { backgroundImage: `url(${site.cover_image})` } }}>
                         <div className="container">
-                            <div className="site-mast">
+                            <nav className="site-mast">
                                 <div className="site-mast-left">
                                     <Link to="/">
                                         <span className="site-logo-title">Bychkow.ski</span>
                                     </Link>
                                 </div>
                                 <div className="site-mast-right">
-                                    { site.twitter && <a href={ twitterUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /></a>}
-                                    { site.facebook && <a href={ facebookUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /></a>}
+                                    <Navigation data={site.navigation} navClass="site-nav-item" />
                                     {/* <a className="site-nav-item" href={ `https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/` } target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a> */}
                                 </div>
-                            </div>
-                            { isHome ?
+                            </nav>
+                            { isHome &&
                                 <div className="site-banner">
                                     <h1 className="site-banner-title">{site.title}</h1>
                                     <p className="site-banner-desc">{site.description}</p>
-                                </div> :
-                                null}
-                            <nav className="site-nav">
-                                <div className="site-nav-left">
-                                    {/* The navigation items as setup in Ghost */}
-                                    <Navigation data={site.navigation} navClass="site-nav-item" />
                                 </div>
-                            </nav>
+                            }
                         </div>
                     </header>
 
